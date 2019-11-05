06b63b7164769e9fe11659bf468ef8916c48800c
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2019-11-05 01:32:45 +0100

    Fix tests

--- ckanext/visualize/tests/test_controller.py
+++ ckanext/visualize/tests/test_controller.py
@@ -8,30 +8,33 @@ from ckanext.visualize.controllers.visualize import VisualizeDataController
 from ckanext.visualize.tests.helpers import mock_pylons
 
 
-class TestVisualizeDataController():
+class TestVisualizeDataController(helpers.FunctionalTestBase):
+
     @classmethod
     def setup_class(self):
+        super(TestVisualizeDataController, self).setup_class()
+
         if not p.plugin_loaded('visualize'):
             p.load('visualize')
 
     @classmethod
     def teardown_class(self):
+        super(TestVisualizeDataController, self).teardown_class()
+
         p.unload('visualize')
 
         helpers.reset_db()
 
     def test_visualize_data_endpoint(self):
+        app = self._get_test_app()
         controller =\
             'ckanext.visualize.controllers.visualize:VisualizeDataController'
         action = 'visualize_data'
         route = url_for(controller=controller, action=action)
-        response = requests.get('http://localhost:5000' + route)
-
-        assert response.ok is True
-        assert response.status_code == 200
+        response = app.get(route)
 
         assert '<section role="main" class="container visualize-wrapper">' in \
-            response.content
+            response.body
 
     def test_visualize_data_raw_action_call(self):
         mock_pylons()
