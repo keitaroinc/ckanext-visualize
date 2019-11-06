c65c850429ae6687d31462daf2fe453264f2453e
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2019-11-06 10:50:45 +0100

    Fetch columns from DataStore

---
+++ ckanext/visualize/helpers.py
@@ -0,0 +1,15 @@
+from ckan.plugins import toolkit
+
+
+def get_fields_without_id(resource):
+    fields = _get_fields(resource)
+    return [{'value': v['id']} for v in fields if v['id'] != '_id']
+
+
+def _get_fields(resource):
+    data = {
+        'resource_id': resource['id'],
+        'limit': 0
+    }
+    result = toolkit.get_action('datastore_search')({}, data)
+    return result['fields']
