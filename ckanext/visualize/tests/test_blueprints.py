816762be609e0b95fad5483fefa78b0b61db47b9
KusevskaElena <elena.kusevska@gmail.com>
2021-03-19 15:10:56 +0100

    test_visualize_data_endpoint_admin[get] passsing

--- ckanext/visualize/tests/test_blueprints.py
+++ ckanext/visualize/tests/test_blueprints.py
@@ -6,9 +6,8 @@ from ckan.tests import helpers, factories
 from ckanext.visualize.views.visualize import visualize_data
 # from ckanext.visualize.tests.helpers import mock_pylons
 #from ckanext.visualize import helpers as h
-# from ckan.plugins.toolkit import url_for
 
-from ckan.lib.helpers import url_for
+from ckan.plugins.toolkit import url_for
 
 #@pytest.mark.ckan_config('ckan.plugins', 'image_view')
 #@pytest.mark.usefixtures('with_plugins')
@@ -88,34 +87,37 @@ class TestVisualizeDataController(object):
 # @pytest.mark.ckan_config('ckan.plugins', 'image_view')
 # @pytest.mark.usefixtures('with_plugins')
 # @pytest.mark.usefixtures('clean_db')
-# class TestAdminController(object):
+class TestAdminController(object):
 
-#     #@classmethod
-#     #def setup_class(self):
-#      #   super(TestAdminController, self).setup_class()
+    def test_visualize_data_endpoint_admin(self, app):
+        user = factories.Sysadmin()
+        env = {'REMOTE_USER': user.get('name').encode('ascii')}
+        url = url_for(u'/visualize_data')
 
-#       #  if not p.plugin_loaded('visualize'):
-#        #     p.load('visualize')
+        dataset = factories.Dataset()
+        resource = factories.Resource(
+            schema='',
+            validation_options='',
+            package_id=dataset.get('id'),
+            datastore_active=True,
+        )
+        resource_id = resource.get('id')
+        data = {
+            'fields': [
+                {'id': 'Age', 'type': 'numeric'},
+                {'id': 'Name', 'type': 'text'},
+            ],
+            'records': [
+                {'Age': 35, 'Name': 'John'},
+                {'Age': 28, 'Name': 'Sara'},
+            ],
+            'force': True,
+            'resource_id': resource_id,
+        }
+        helpers.call_action('datastore_create', **data)
+        response = app.get(url=(url + '?resource_id={0}'.format(resource_id)), extra_environ=env)
 
-#     #@classmethod
-#     #def teardown_class(self):
-#      #   super(TestAdminController, self).teardown_class()
-
-#      #   p.unload('visualize')
-
-#       #  helpers.reset_db()
-
-#     def test_visualize_data_endpoint_admin(self, app):
-#         #app = self._get_test_app()
-#         user = factories.Sysadmin()
-#         env = {'REMOTE_USER': user.get('name').encode('ascii')}
-#         controller =\
-#             'ckanext.visualize.views.admin_visualize:AdminController'
-#         action = 'visualize_data'
-#         url = h.url_for(u'blueprints.blueprints')
-#         response = app.get(url=url, extra_environ=env)
-
-#         assert 'Visualize' in response.body
+        assert '<div class="chart-container hidden">' in response.body
 
 #     @helpers.change_config('visualize_colors', '{"color_1":"#fff"}')
 #     def test_visualize_data_endpoint_predefined_color(self, app):
@@ -130,22 +132,22 @@ class TestVisualizeDataController(object):
 
 #         assert 'Visualize' in response.body
 
-#     def test_visualize_data_endpoint_post(self, app):
-#         #app = self._get_test_app()
-#         user = factories.Sysadmin()
-#         env = {'REMOTE_USER': user.get('name').encode('ascii')}
-#         controller =\
-#             'ckanext.visualize.views.admin_visualize:AdminController'
-#         action = 'visualize_data'
-#         url = h.url_for(u'blueprints.blueprints')
-#         params = {
-#             'save': True,
-#             'color_1': '#332288',
-#             'color_2': '#117733',
-#         }
-#         response = app.post(url=url, extra_environ=env, params=params)
+    def test_visualize_data_endpoint_post(self, app):
+        #app = self._get_test_app()
+        user = factories.Sysadmin()
+        env = {'REMOTE_USER': user.get('name').encode('ascii')}
+        #controller =\
+        #    'ckanext.visualize.views.admin_visualize:AdminController'
+        #action = 'visualize_data'
+        url = url_for(u'/visualize_data')
+        params = {
+            'save': True,
+            'color_1': '#332288',
+            'color_2': '#117733',
+        }
+        response = app.post(url=url, extra_environ=env, params=params)
 
-#         assert '302 Found' in response.body
+        assert '302 Found' in response.body
 
 #     def test_visualize_data_endpoint_post_icon_url(self):
 #         #app = self._get_test_app()
