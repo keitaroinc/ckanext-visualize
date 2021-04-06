02c70639436eae90d4d1d55f427859438968c55d
Elena Kusevska <31021042+ElenaKusevska@users.noreply.github.com>
2021-04-06 18:29:06 +0200

    Update backend.ckan.js

--- ckanext/visualize/assets/js/vendor/backend.ckan.js
+++ ckanext/visualize/assets/js/vendor/backend.ckan.js
@@ -1,23 +1,3 @@
-/*
-Original source code modified by Keitaro AB
-Copyright (c) 2019 Keitaro AB
-********************************************************************
-Copyright (c) 2006-2018 Open Knowledge Foundation and contributors
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 this.recline = this.recline || {};
 this.recline.Backend = this.recline.Backend || {};
 this.recline.Backend.Ckan = this.recline.Backend.Ckan || {};
